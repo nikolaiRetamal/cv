8630ce71e6dbac2351089881f19983e64f74c8f5
nikolaiRetamal <nikolai.retamal@yahoo.fr>
2017-02-20 15:35:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+cv_2017
+=======
+
+A Symfony project created on February 20, 2017, 3:17 pm.
---
+++ src/AppBundle/Controller/AccueilNiko.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AccueilNiko extends Controller{
+    /**
+     * @Route("/accueil")
+     */
+    public function accueilNiko()
+    {
+		return $this->render('cvNiko.html.twig', array(
+					'number' => 0,
+				));
+    }
+}
+?>
