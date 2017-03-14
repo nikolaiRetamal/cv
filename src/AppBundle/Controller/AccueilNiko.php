5076eac91e5454cea9bb83f142a64246e46a4851
nikolaiRetamal <nikolai.retamal@yahoo.fr>
2017-03-14 12:00:42 +0100

    Maj Controller

--- src/AppBundle/Controller/AccueilNiko.php
+++ src/AppBundle/Controller/AccueilNiko.php
@@ -11,8 +11,11 @@ class AccueilNiko extends Controller{
      */
     public function accueilNiko()
     {
+		
+		$content = file_get_contents('./js/data_nikolai.json');
+		$json = json_decode($content, true);
 		return $this->render('cv.html.twig', array(
-					'number' => 0,
+					'info' => $json,
 				));
     }
 }
